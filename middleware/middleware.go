87d3acf1470107d9373917375bfaac33de454c62
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2020-05-11 20:47:42 -0300

    implement middlware with errors validation, insertion errors and add p[agination + querying

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"reflect"
 	"time"
 
 	"../models"
@@ -15,9 +14,10 @@ import (
 	"gopkg.in/go-playground/validator.v9"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	. "github.com/gobeam/mongo-go-pagination"
 )
 
 // DB connection string, if local use localhost below
@@ -36,6 +36,7 @@ const collName = "books"
 var collection *mongo.Collection
 
 func init() {
+	fmt.Println(connectionString)
 	clientOptions := options.Client().ApplyURI(connectionString)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
@@ -62,13 +63,24 @@ func init() {
 // DB Functions - CRUDDING
 
 // get all books
-// todo: add params
-// todo: pagination
 func GetAllBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	payload := getAllBooks()
-	json.NewEncoder(w).Encode(payload)
+
+	page, limit := utils.Pagination(r)
+	filter := utils.Filter(r)
+
+	payload, paginationInfo := getAllBooks(limit, page, filter)
+	fmt.Println(paginationInfo)
+	answer := models.GetAnswer{
+		Ok:     true,
+		Data:   payload,
+		Page: paginationInfo.Page,
+		Total: paginationInfo.Total,
+		PerPage: paginationInfo.PerPage,
+
+	}
+	json.NewEncoder(w).Encode(answer)
 }
 
 func RegisterBook(w http.ResponseWriter, r *http.Request) {
@@ -82,25 +94,47 @@ func RegisterBook(w http.ResponseWriter, r *http.Request) {
 	v := validator.New()
 	err := v.Struct(book)
 
-	// todo: break condition if err
-	fmt.Println("validation-err: ", err)
+	var errs []string
+	var answer models.Answer
 
-	for _, e := range err.(validator.ValidationErrors) {
-		fmt.Println(e)
+	if err != nil {
+		for _, e := range err.(validator.ValidationErrors) {
+			errs = append(errs, utils.HandleDbError(e))
+		}
+		fmt.Println("validated...")
+		answer = models.Answer{
+			Ok:     false,
+			Errors: errs,
+		}
+		fmt.Println(errs)
+		w.WriteHeader(400)
+	} else {
+		book.ID = book.ISBN
+		if _, insertError := insertBook(book); insertError != "" {
+			w.WriteHeader(409)
+			answer = models.Answer{
+				Ok: false,
+				Errors: append(errs, insertError),
+			}
+		} else {
+			answer = models.Answer{
+				Ok:     true,
+				Errors: nil,
+				Data:   &book,
+			}
+			fmt.Println(book, r.Body)
+		}
+
 	}
 
-	fmt.Println(book, r.Body)
-	insertBook(book)
-	json.NewEncoder(w).Encode(book)
+	json.NewEncoder(w).Encode(answer)
 }
 
 func GetBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	// todo: check for isbn or id
 	params := mux.Vars(r)
-	//book := getBook(params["ID"])
 	book := getBook(params["ISBN"])
 	json.NewEncoder(w).Encode(book)
 }
@@ -112,45 +146,44 @@ func DeleteBook (w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 	params := mux.Vars(r)
-	//deleteBook(params["ID"])
 	deleteBook(params["ISBN"])
-	json.NewEncoder(w).Encode(params["id"])
+
+	json.NewEncoder(w).Encode(models.Answer{ Ok: true })
 }
 
-// todo: add pagination
-func getAllBooks() []primitive.M {
+func getAllBooks(limit int, page int, filter bson.M) ([]models.Book, PaginationData) {
 
-	cur, err := collection.Find(context.Background(), bson.D{{}})
+
+	projection := bson.D{
+		{"title", 1},
+		{"author", 1},
+		{"isbn", 1},
+		{"publisher", 1},
+		{"year", 1},
+		{"language", 1},
+	}
+
+	// sorted data use below
+	// paginatedData, err := New(collection).Limit(limit).Page(page).Sort("year", -1).Select(projection).Filter(filter).Find()
+	paginatedData, err := New(collection).Limit(int64(limit)).Page(int64(page)).Select(projection).Filter(filter).Find()
 	if err != nil {
+		fmt.Println("Error on paginated data /getallbooks")
 		log.Fatal(err)
 	}
-
-	var results []primitive.M
-	fmt.Println("cur books... :")
-	fmt.Println(cur)
-	for cur.Next(context.Background()) {
-		var result bson.M
-		e := cur.Decode(&result)
-		if e != nil {
-			log.Fatal(e)
+	var lists []models.Book
+	for _, raw := range paginatedData.Data {
+		var book *models.Book
+		if marshallErr := bson.Unmarshal(raw, &book); marshallErr == nil {
+			lists = append(lists, *book)
 		}
-		fmt.Println("cur..>", cur, "result", reflect.TypeOf(result), reflect.TypeOf(result["_id"]))
-		results = append(results, result)
-
 	}
 
-	if err := cur.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	cur.Close(context.Background())
-	return results
-
+	return lists, paginatedData.Pagination
 }
 
 func getBook(ISBN string) models.Book {
 	var book models.Book
-	filter := bson.M{"ISBN": ISBN}
+	filter := bson.M{"_id": ISBN}
 	if err := collection.FindOne(context.Background(), filter).Decode(&book); err != nil {
 		log.Fatal(err)
 	}
@@ -159,21 +192,24 @@ func getBook(ISBN string) models.Book {
 	return book
 }
 
-func insertBook(book models.Book) {
+func insertBook(book models.Book) (interface{}, string) {
 	insertResult, err := collection.InsertOne(context.Background(), book)
 
+	var errmsg = ""
+	var insertedId interface{} = nil
+
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		errmsg = utils.HandleWriteError(err)
+	} else {
+		insertedId = insertResult.InsertedID
 	}
 
-	fmt.Println("inserted a single record", insertResult)
+	return insertedId, errmsg
 }
 
 func deleteBook(ISBN string) {
-	fmt.Println(ISBN)
-	//id, _ := primitive.ObjectIDFromHex(task)
-	//filter := bson.M{"_id": id}
-	filter := bson.M{"ISBN": ISBN}
+	filter := bson.M{"_id": ISBN}
 	d, err := collection.DeleteOne(context.Background(), filter)
 
 	if err != nil {
