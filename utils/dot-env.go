ea3850e96bc0ea41b5a7be6daad89561d49f32a3
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2020-05-11 13:13:16 -0300

    create utils to read .env files

--- utils/dot-env.go
+++ utils/dot-env.go
@@ -1,1 +1,27 @@
 package utils
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+// load .env file
+var err = godotenv.Load("./server/.env")
+
+
+func GetEnvVariable(key string) string {
+
+	if err != nil {
+		fmt.Println(err)
+		log.Fatalf("Error loading .env file")
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
