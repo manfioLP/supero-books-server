55e0fa5957734a7ecd5998aebc48967903873279
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2020-05-11 13:13:27 -0300

    create router

--- router/router.go
+++ router/router.go
@@ -1,1 +1,17 @@
 package router
+
+import (
+	"../middleware"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/book", middleware.GetAllBooks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/book", middleware.RegisterBook).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/book/{ISBN}", middleware.GetBook).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/book/{ISBN}", middleware.DeleteBook).Methods("DELETE", "OPTIONS")
+	return router
+}
