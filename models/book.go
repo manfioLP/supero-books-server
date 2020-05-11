b9867e6dbdc420be71a97edbc50d0bcd6c44fe36
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2020-05-11 11:36:35 -0300

    fix typo on struct name

--- models/book.go
+++ models/book.go
@@ -1,6 +1,6 @@
 package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
-type ToDoList struct {
+type Book struct {
 
 	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Title   string             `json:"title" validate:"required,min=2,max=100"`
